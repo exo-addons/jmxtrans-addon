6e58388c5fb690d911defe1f9c749c194ad46a0a
Frédéric DROUET <fdrouet@exoplatform.com>
2016-03-16 22:45:47 +0100

    Formating

--- lib/src/main/java/org/exoplatform/addons/jmxtrans/JMXTransLoaderListener.java
+++ lib/src/main/java/org/exoplatform/addons/jmxtrans/JMXTransLoaderListener.java
@@ -27,6 +27,7 @@ import javax.management.MBeanServer;
 import javax.management.ObjectName;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
 import org.jmxtrans.embedded.EmbeddedJmxTrans;
 import org.jmxtrans.embedded.EmbeddedJmxTransException;
 import org.jmxtrans.embedded.config.ConfigurationParser;
@@ -39,84 +40,84 @@ import org.slf4j.LoggerFactory;
  */
 public class JMXTransLoaderListener implements ServletContextListener {
 
-  /**
-   * Config param for the embedded-jmxtrans configuration urls.
-   */
-  public static final String CONFIG_LOCATION_PARAM = "jmxtrans.config";
-  public static final String SYSTEM_CONFIG_LOCATION_PARAM = "jmxtrans.system.config";
-  protected final Logger logger = LoggerFactory.getLogger(getClass());
-  private EmbeddedJmxTrans embeddedJmxTrans;
-  private ObjectName objectName;
-  private MBeanServer mbeanServer;
+    /**
+     * Config param for the embedded-jmxtrans configuration urls.
+     */
+    public static final String CONFIG_LOCATION_PARAM = "jmxtrans.config";
+    public static final String SYSTEM_CONFIG_LOCATION_PARAM = "jmxtrans.system.config";
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+    private EmbeddedJmxTrans embeddedJmxTrans;
+    private ObjectName objectName;
+    private MBeanServer mbeanServer;
 
-  @Override
-  public void contextInitialized(ServletContextEvent sce) {
-    sce.getServletContext().log("Start embedded-jmxtrans ...");
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        sce.getServletContext().log("Start embedded-jmxtrans ...");
 
-    mbeanServer = ManagementFactory.getPlatformMBeanServer();
+        mbeanServer = ManagementFactory.getPlatformMBeanServer();
 
-    ConfigurationParser configurationParser = new ConfigurationParser();
+        ConfigurationParser configurationParser = new ConfigurationParser();
 
-    String configuration = configureFromSystemProperty(sce);
-    if (configuration == null || configuration.isEmpty()) {
-      configuration = configureFromWebXmlParam(sce);
-      if (configuration == null || configuration.isEmpty()) {
-        configuration = "classpath:jmxtrans.json, classpath:org/jmxtrans/embedded/config/jmxtrans-internals.json";
-      }
+        String configuration = configureFromSystemProperty(sce);
+        if (configuration == null || configuration.isEmpty()) {
+            configuration = configureFromWebXmlParam(sce);
+            if (configuration == null || configuration.isEmpty()) {
+                configuration = "classpath:jmxtrans.json, classpath:org/jmxtrans/embedded/config/jmxtrans-internals.json";
+            }
+        }
+
+        sce.getServletContext().log("Embedded JMXTrans configuration : " + configuration);
+
+        List<String> configurationUrls = StringUtils2.delimitedStringToList(configuration);
+        embeddedJmxTrans = configurationParser.newEmbeddedJmxTrans(configurationUrls);
+        String on = "org.jmxtrans.embedded:type=EmbeddedJmxTrans,name=jmxtrans,path=" + sce.getServletContext().getContextPath();
+        try {
+            objectName = mbeanServer.registerMBean(embeddedJmxTrans, new ObjectName(on)).getObjectName();
+        } catch (Exception e) {
+            throw new EmbeddedJmxTransException("Exception registering '" + objectName + "'", e);
+        }
+        try {
+            embeddedJmxTrans.start();
+        } catch (Exception e) {
+            String message = "Exception starting jmxtrans for application '" + sce.getServletContext().getContextPath() + "'";
+            sce.getServletContext().log(message, e);
+            throw new EmbeddedJmxTransException(message, e);
+        }
     }
 
-    sce.getServletContext().log("Embedded JMXTrans configuration : "+configuration);
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        sce.getServletContext().log("Stop embedded-jmxtrans ...");
 
-    List<String> configurationUrls = StringUtils2.delimitedStringToList(configuration);
-    embeddedJmxTrans = configurationParser.newEmbeddedJmxTrans(configurationUrls);
-    String on = "org.jmxtrans.embedded:type=EmbeddedJmxTrans,name=jmxtrans,path=" + sce.getServletContext().getContextPath();
-    try {
-      objectName = mbeanServer.registerMBean(embeddedJmxTrans, new ObjectName(on)).getObjectName();
-    } catch (Exception e) {
-      throw new EmbeddedJmxTransException("Exception registering '" + objectName + "'", e);
-    }
-    try {
-      embeddedJmxTrans.start();
-    } catch (Exception e) {
-      String message = "Exception starting jmxtrans for application '" + sce.getServletContext().getContextPath() + "'";
-      sce.getServletContext().log(message, e);
-      throw new EmbeddedJmxTransException(message, e);
-    }
-  }
-
-  @Override
-  public void contextDestroyed(ServletContextEvent sce) {
-    sce.getServletContext().log("Stop embedded-jmxtrans ...");
-
-    try {
-      mbeanServer.unregisterMBean(objectName);
-    } catch (Exception e) {
-      logger.error("Silently skip exception unregistering mbean '" + objectName + "'");
-    }
-    try {
-      embeddedJmxTrans.stop();
-    } catch (Exception e) {
-      throw new EmbeddedJmxTransException("Exception stopping '" + objectName + "'", e);
-    }
-  }
-
-  private String configureFromSystemProperty(ServletContextEvent sce) {
-    String configSystemProperty =
-        sce.getServletContext().getInitParameter(SYSTEM_CONFIG_LOCATION_PARAM);
-
-    if (configSystemProperty == null || configSystemProperty.isEmpty()) {
-      return null;
+        try {
+            mbeanServer.unregisterMBean(objectName);
+        } catch (Exception e) {
+            logger.error("Silently skip exception unregistering mbean '" + objectName + "'");
+        }
+        try {
+            embeddedJmxTrans.stop();
+        } catch (Exception e) {
+            throw new EmbeddedJmxTransException("Exception stopping '" + objectName + "'", e);
+        }
     }
 
-    String prop = System.getProperty(configSystemProperty);
-    if (prop == null || prop.isEmpty()) {
-      return null;
+    private String configureFromSystemProperty(ServletContextEvent sce) {
+        String configSystemProperty =
+                sce.getServletContext().getInitParameter(SYSTEM_CONFIG_LOCATION_PARAM);
+
+        if (configSystemProperty == null || configSystemProperty.isEmpty()) {
+            return null;
+        }
+
+        String prop = System.getProperty(configSystemProperty);
+        if (prop == null || prop.isEmpty()) {
+            return null;
+        }
+
+        return prop;
     }
 
-    return prop;
-  }
-
-  private String configureFromWebXmlParam(ServletContextEvent sce) {
-    return sce.getServletContext().getInitParameter(CONFIG_LOCATION_PARAM);
-  }
+    private String configureFromWebXmlParam(ServletContextEvent sce) {
+        return sce.getServletContext().getInitParameter(CONFIG_LOCATION_PARAM);
+    }
 }
